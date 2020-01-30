0e1bb3777708bd3e21ec24de2b9cfd87215c1dea
Alex C-G <alexcg@outlook.com>
2020-01-31 12:33:37 +1300

    Basic functionality

---
+++ tweets-to-jekyll.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import twitter
+import config
+import re
+from datetime import datetime, timedelta
+
+username = config.username
+
+api = twitter.Api(
+    consumer_key = config.consumer_key,
+    consumer_secret = config.consumer_secret,
+    access_token_key = config.access_token_key,
+    access_token_secret = config.access_token_secret)
+
+def get_tweets(timeframe = 7):
+    statuses = api.GetUserTimeline(screen_name=username, exclude_replies=True)
+
+    formatted_tweet_list = []
+
+
+    timeframe = datetime.combine(datetime.now().date() - timedelta(days = timeframe),datetime.min.time())
+
+    # Regexes to find RT's and tweets w/ URLs
+    url_format = ' https:\/\/t.co\/.*'
+    rt_format = r'RT @.*:'
+
+    for tweet in [s for s in statuses]:
+        tweet_year = tweet.created_at[-4:]
+        tweet_date = tweet.created_at[:-20]
+        tweet_date = tweet_date + ' ' + tweet_year
+        tweet_date = datetime.strptime(tweet_date, '%a %b %d %Y')
+        if tweet_date < timeframe:
+            break
+
+        text = tweet.text.replace("\n", " ")
+        text = re.sub(url_format, '', text, flags=re.MULTILINE)
+        via = re.findall(rt_format, text)
+        text = re.sub(rt_format+' ', '', text)
+
+        if tweet.quoted_status:
+            url = tweet.quoted_status.urls[0].url
+        elif tweet.urls:
+            url = tweet.urls[0].url
+
+        formatted_tweet = f"* [{text}]({url})"
+
+        if via:
+            via = re.sub('RT ', '', via[0])
+            via = ' via ' + re.sub(':', '', via)
+            formatted_tweet += via
+
+        formatted_tweet_list.append(formatted_tweet)
+
+    for tweet in formatted_tweet_list:
+        print(tweet)
+    return formatted_tweet_list
+
+
+def write_tweets_to_file(tweets):
+    count = 0
+    with open('tweets.md', "w") as file:
+        for tweet in tweets:
+            file.write(tweet+'\n')
+            count += 1
+        print(count, "tweets written to file")
+
+tweets = get_tweets()
+write_tweets_to_file(tweets)
+
