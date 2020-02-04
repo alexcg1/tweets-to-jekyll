63fe4f0654e82063a9a832d253023bce021750b5
Alex C-G <alexcg@outlook.com>
2020-02-04 16:40:21 +1300

    Plain text logging; general fixes

--- tweets-to-jekyll.py
+++ tweets-to-jekyll.py
@@ -1,33 +1,82 @@
 #!/usr/bin/env python3
 
-try:
-    import config
-except:
-    print("Error: No config file found")
+import sys
+import sqlite3
+import config
+from pprint import pprint
+import logging as log
+from datetime import datetime, timedelta
+
+log.basicConfig(format="* %(levelname)s: %(message)s", level=log.INFO)
+log.info("Verbose output.")
+
+simple_tweets = []
+tweets_written = 0
+tweets_skipped = 0
 
 
 def get_tweets():
+    '''
+    Get tweets from user account
+    '''
+    import credentials
     import twitter
-    api = twitter.Api(
-        consumer_key = config.consumer_key,
-        consumer_secret = config.consumer_secret,
-        access_token_key = config.access_token_key,
-        access_token_secret = config.access_token_secret)
 
-    print(f"* Retrieving tweets from user {config.username}")
     try:
-        tweets = api.GetUserTimeline(screen_name=config.username, exclude_replies=config.exclude_replies)
-        print(f"* {len(tweets)} tweets received")
+        get_last_tweet_id()
     except:
-        print("* ERROR: Failed to retrieve tweets")
+        pass
+
+    api = twitter.Api(
+        consumer_key = credentials.consumer_key,
+        consumer_secret = credentials.consumer_secret,
+        access_token_key = credentials.access_token_key,
+        access_token_secret = credentials.access_token_secret)
+
+    try:
+        tweets = api.GetUserTimeline(
+            screen_name=config.username,
+            exclude_replies=config.exclude_replies,
+            count=config.tweet_limit
+        )
+        log.info(f"{len(tweets)} tweets received from {config.username}")
+    except:
+        log.warn("Failed to retrieve tweets")
+
     return tweets
 
 
+def get_last_tweet_id():
+    '''
+    Get the id field of the user's most recent tweet from the log
+    '''
+
+    try:
+        with open(config.tweet_log, "r") as file:
+            tweet_id = file.readlines()[0]
+    except:
+        pass
+
+    # with sqlite3.connect(config.database) as conn:
+    # cur = conn.cursor()
+    # cur.execute("SELECT id FROM Tweets")
+    # db_output = cur.fetchall()
+
+    # tweet_id = db_output[-1][0]
+    # for row in db_output:
+    # done_tweet_list.append(row[0])
+    # counter += 1
+    return tweet_id
+
+
 def filter_tweets(tweets, timeframe=config.default_timeframe):
-    from datetime import datetime, timedelta
+    '''
+    Filter tweets based on timeframe and (pending) other criteria
+    '''
     filtered_tweets = []
     timeframe = datetime.combine(datetime.now().date() - timedelta(days = timeframe),datetime.min.time())
     for tweet in [s for s in tweets]:
+        tweet.done = False
         tweet_year = tweet.created_at[-4:]
         tweet_date = tweet.created_at[:-20]
         tweet_date = tweet_date + ' ' + tweet_year
@@ -38,10 +87,15 @@ def filter_tweets(tweets, timeframe=config.default_timeframe):
 
 
 def format_tweets(tweets):
+    '''
+    Convert tweet dict to array of markdown bullet points
+    '''
     import re
+    import json
     formatted_tweet_list = []
     url_format = ' https:\/\/t.co\/.*'
     rt_format = r'RT @.*:'
+    counter = 0
 
     for tweet in [s for s in tweets]:
         text = tweet.text.replace("\n", " ")
@@ -49,59 +103,140 @@ def format_tweets(tweets):
         via = re.findall(rt_format, text)
         text = re.sub(rt_format+' ', '', text)
 
-        if tweet.quoted_status:
-            url = tweet.quoted_status.urls[0].url
-        elif tweet.urls:
-            url = tweet.urls[0].url
+        # Set default url
+        tweet.url = f"https://twitter.com/{config.username}/statuses/{tweet.id}"
 
-        tweet.formatted = f"* [{text}]({url})"
-        # tweet.formatted = formatted_tweet
+        # Better URL handling - get direct link if link mentioned in tweet (e.g. to YouTube or whatever)
+        # if tweet.media:
+        # print("Media")
+        # # url = tweet.retweeted_status.urls[0].url
+        # print(tweet.id_str)
+        # url = ''
+        # elif tweet.retweeted_status:
+        # print("Retweet")
+        # # url = tweet.retweeted_status.urls[0].url
+        # print(tweet.retweeted_status.media)
+        # url = ''
+        # elif tweet.quoted_status:
+        # try:
+        # url = tweet.quoted_status.urls[0].url
+        # except:
+        # url = ''
+        # elif tweet.urls:
+        # url = tweet.urls[0].url
+        # else:
+        # url = ''
+
+        tweet.formatted = f"* [{text}]({tweet.url})"
 
         if via:
             via = re.sub('RT ', '', via[0])
             via = ' via ' + re.sub(':', '', via)
             tweet.formatted += via
+            tweet.via = via
 
-        formatted_tweet_list.append(tweet.formatted)
+        simple_tweet = {"id": tweet.id, "url": tweet.url, "text": tweet.text, "formatted": tweet.formatted, "timestamp": 0, "done": False}
 
-    return formatted_tweet_list
+        simple_tweets.append(simple_tweet)
 
 
-def write_tweets_to_file(tweets):
+def write_tweets_to_file(simple_tweets):
+    '''
+    Write header, footer, and each bullet point to a markdown file.
+    '''
     header = f"---\ntitle: {config.title}\n---\n\n"
     footer = "\nCreated with tweets-to-jekyll"
     count = 0
     body = ''
 
-    for tweet in tweets:
-        body += tweet+'\n'
+    for tweet in simple_tweets:
+        # tweet["done"] = False
+        body += tweet["formatted"]+'\n'
         count += 1
+        # tweet["done"] = True
+        # print("-"*100)
+        # pprint(tweet)
+        write_log(tweet)
 
     with open(config.filename, "w") as file:
         file.write(header)
         file.write(body)
         file.write(footer)
 
-    print(f"* {count} tweets written to {config.filename}")
+    log.info(str(count) + " tweets written to " + config.filename)
 
 
 def post_to_jekyll(file):
+    '''
+    Move the file to your jekyll posts directory
+    '''
     pass
 
 
-def mark_posted():
-    import sqlite3
+def __check_log_for_tweet(tweet):
+    '''
+    Check if tweet already logged in file
+    '''
+    try:
+        with open(config.tweet_log, "r") as file:
+            if str(tweet["id"])+"\n" in file.readlines():
+                return True
+    except:
+        pass
 
-    with sqlite3.connect("tweets.sqlite3") as conn:
-        command = "INSERT INTO Tweets VALUES (?, ?)"
+    # done_tweet_list = []
+    # counter = 0
 
-        for i in range(5):
-            conn.execute(command, (12345, 123))
-            conn.commit()
+    # with sqlite3.connect(config.database) as conn:
+    # cur = conn.cursor()
+    # cur.execute("SELECT id FROM Tweets")
+    # db_output = cur.fetchall()
+
+    # for row in db_output:
+    # done_tweet_list.append(row[0])
+    # counter += 1
+
+    # if tweet["id"] in done_tweet_list:
+    # return True
+    # else:
+    # return False
+
+
+def write_log(tweet):
+    '''
+    Mark tweet as processed in the database
+    '''
+    if __check_log_for_tweet(tweet) == True:
+        pass
+
+    else:
+        with open("posted_tweets.log", "a") as file:
+            file.write(str(tweet["id"])+"\n")
+    # log.info(str(tweet["id"]) + " already processed. Skipping")
+    # tweets_skipped += 1
+    # else:
+    # tweet["done"] = 1
+    # tweet["timestamp"] = datetime.timestamp(datetime.now())
+
+    # with sqlite3.connect("tweets.sqlite3") as conn:
+    # command = "INSERT INTO Tweets VALUES (?, ?, ?, ?)"
+    # conn.execute(command, (tweet["id"], tweet["done"], tweet["timestamp"], tweet["url"]))
+
+    # # tweets_written += 1
+
+    # conn.commit()
+
+
+
 
 if __name__ == "__main__":
+
     # mark_posted()
     tweets = get_tweets()
     filtered_tweets = filter_tweets(tweets)
     formatted_tweets = format_tweets(filtered_tweets)
-    write_tweets_to_file(formatted_tweets)
+    # pprint(simple_tweets)
+    # write_tweets_to_file(formatted_tweets)
+    write_tweets_to_file(simple_tweets)
+    # print(f"{tweets_written} tweets written to {config.filename}. {tweets_skipped} skipped")
+
